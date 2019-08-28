6457d3658d3b5a473cbf8749a9808a08c9e51be9
michdrw <michdrw@gmail.com>
2019-08-28 10:39:25 -0300

    Hechizos agregados por Michelle

--- src/app/App.java
+++ src/app/App.java
@@ -7,11 +7,12 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Inicializando Catalogo de personajes");
         DueloMagico.inicializarCatalogoPersonajes();
-        
+
         IHacerMagia personajeElegido = (IHacerMagia) DueloMagico.elegirPersonaje("Fred Weasley");
         IHacerMagia enemigoElegido = (IHacerMagia) DueloMagico.elegirPersonaje("George Weasley");
         //Poder poderelegido = personajeelegido.get 
         
+        personajeElegido.aprender(hechizos.ha1);
         personajeElegido.atacar(enemigoElegido, ha1);
     }
 }
