302d06a323490164f96fce4a78b65ecce43a5ce4
lucilaada <lucilasol.aguilar01>
2019-08-30 12:48:05 -0300

    correcion de errores lucila

--- src/app/hechizos/defensa/CaveInmicum.java
+++ src/app/hechizos/defensa/CaveInmicum.java
@@ -8,5 +8,7 @@ public class CaveInmicum extends HechizoDefensa {
     public CaveInmicum(String nombre, double nivelDaño, double nivelCuracion) {
         super(nombre, nivelDaño, nivelCuracion);
     }
-
+    public CaveInmicum() {
+        super("CaveInmicum", 0, 0);
+    }
 }
--- src/app/hechizos/defensa/Protego.java
+++ src/app/hechizos/defensa/Protego.java
@@ -8,5 +8,7 @@ public class Protego extends HechizoDefensa {
     public Protego(String nombre, double nivelDaño, double nivelCuracion) {
         super(nombre, nivelDaño, nivelCuracion);
     }
-
+    public Protego() {
+        super("Protego", 0, 0);
+    }
 }
