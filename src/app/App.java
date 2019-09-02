2e90ede57bd6f0278853351ec791a6c9af3bf224
michdrw <michdrw@gmail.com>
2019-09-02 11:39:34 -0300

    Mini cambios en App y Wizard

--- src/app/App.java
+++ src/app/App.java
@@ -17,14 +17,17 @@ public class App {
                 
                 Personaje personajeElegido = (Personaje) DueloMagico.elegirPersonaje1("Fred Weasley");
 
-                Personaje enemigoElegido = (Personaje)  DueloMagico.elegirPersonaje2("George Weasley");
+                Personaje enemigoElegido = (Personaje) DueloMagico.elegirPersonaje2("George Weasley");
 
-                System.out.println("Que comience el duelo: ");
+                System.out.println("¡Comienza el duelo!");
                 while (personajeElegido.salud > 0 && enemigoElegido.salud > 0) {
                         if (personajeElegido instanceof Wizard && ((Wizard) personajeElegido).energiaMagica > 0)  {
+                                System.out.println("Turno de : " + personajeElegido.nombre );
+                                
                                 ((IHacerMagia) personajeElegido).atacar(enemigoElegido,((Wizard) personajeElegido).getHechizo());
                         }
                         if (enemigoElegido instanceof Wizard && ((Wizard) enemigoElegido).energiaMagica > 0) {
+                                System.out.println("Turno de : " + enemigoElegido.nombre);
                                 ((IHacerMagia) enemigoElegido).atacar(personajeElegido,((Wizard) enemigoElegido).getHechizo());
                         }
 
--- src/app/personajes/persona/Wizard.java
+++ src/app/personajes/persona/Wizard.java
@@ -75,6 +75,7 @@ public class Wizard extends Persona implements IHacerMagia {
     }
 
     public Artefacto getArtefacto() {
+        
         return this.artefacto;
 
     }
